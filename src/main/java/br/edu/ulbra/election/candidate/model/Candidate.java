557c33fb5a3c49b2c27e28e6e362eb57d4466b94
Rafael <rafaelmsabino@gmail.com>
2018-11-05 17:40:02 -0200

    canditate entity and repository

---
+++ src/main/java/br/edu/ulbra/election/candidate/model/Candidate.java
@@ -0,0 +1,64 @@
+package br.edu.ulbra.election.candidate.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Candidate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @Column(nullable = false)
+    private Long number;
+    
+    @Column(nullable = false)
+    private Long election_id;
+    
+    @Column(nullable = false)
+    private Long party_id;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getNumber() {
+        return number;
+    }
+
+    public void setNumber(Long number) {
+        this.number = number;
+    }
+
+    public Long getElection_id() {
+        return election_id;
+    }
+
+    public void setElection_id(Long election_id) {
+        this.election_id = election_id;
+    }
+
+    public Long getParty_id() {
+        return party_id;
+    }
+
+    public void setParty_id(Long party_id) {
+        this.party_id = party_id;
+    }
+}
